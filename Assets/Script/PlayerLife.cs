a146867088a37d5cde8a391c8fbe7cbdf788f5a3
alfonsosun <alfiesunn666@gmail.com>
2024-11-12 12:49:03 -0800

    Added Life count and Spikes trap inside

---
+++ Assets/Script/PlayerLife.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void OnCollisionEneter2D(Collision2D collision)
+    {
+
+    }
+}
