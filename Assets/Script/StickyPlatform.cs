cf3384b61583ef5a622843bd648e757dcaaf573b
alfonsosun <alfiesunn666@gmail.com>
2024-11-14 01:19:32 -0800

    Fixed movingPlatform and include Prefabs

--- Assets/Script/StickyPlatform.cs
+++ Assets/Script/StickyPlatform.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class StickyPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.name == "Player")
+    //    {
+    //        collision.gameObject.transform.SetParent(transform);
+    //    }
+    //}
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.name == "Player")
+    //    {
+    //        collision.gameObject.transform.SetParent(null);
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
     }
 }
