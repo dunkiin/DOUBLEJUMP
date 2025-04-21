ba3698e2bd7fbbdd1fc4c849d8e652a7a0270e9c
alfonsosun <alfiesunn666@gmail.com>
2025-04-21 03:50:44 -0700

    Update PausePopup

--- Assets/Script/PausePopup.cs
+++ Assets/Script/PausePopup.cs
@@ -1,16 +1,60 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PausePopup : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private KeyCode pauseKey = KeyCode.Escape;
+
+    private bool isPaused = false;
+
+
+    private void Start()
     {
-        
+        // Ensure hidden and unpaused at start
+        isPaused = false;
+        Time.timeScale = 1f;
+
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Only listen if we have a panel to toggle
+        if (pausePanel == null) return;
+
+        // Toggle pause when the configured key is pressed
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if (isPaused)
+                OnReturnToGameButton();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        Debug.Log("Game Paused");
+    }
+
+    public void OnReturnToGameButton()
+    {
+        isPaused = false;
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        Debug.Log("Game Resumed");
+    }
+
+    public void OnExitGameButton()
+    {
+        Debug.Log("exit game");
+        Application.Quit();
     }
 }
